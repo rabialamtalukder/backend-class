38794e600ff799f0eda5794a06c6945cdd6293cd
rabi alam <rabi_004@hotmail.com>
2020-04-14 23:38:35 -0500

    Initial commit

---
+++ src/main/java/com/example/backendclass/BackendClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendclass/BackendClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
